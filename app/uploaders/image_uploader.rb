913e96864a8eb6d4e06ff160cb55ea7fa09e0db5
lillycoder <ltheinh@gmail.com>
2019-07-16 23:02:07 +0000

    finished lesson 24 part I

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,7 @@ include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   #storage :file
-  storage :fog
+  storage :aws
   process resize_to_fill: [800, 350]
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
