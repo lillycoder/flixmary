a6f56124ff73d44d68e3f433bdc56eb28ca4da17
lillycoder <ltheinh@gmail.com>
2019-07-16 21:39:06 +0000

    forgot a comma in my yml file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 #config/initializers/carrierwave.rb
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider =>'AWS'
+    :provider =>'AWS',
     :aws_access_key_id => ENV["AWS_ACCESS_KEY"],
     :aws_secret_access_key => ENV["AWS_SECRET_KEY"],
   }
