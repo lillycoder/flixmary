3c1682be075c3c59e2ce19d3f76f9b9bf29c2a63
lillycoder <ltheinh@gmail.com>
2019-07-18 00:09:41 +0000

    finished lesson 28

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,18 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show] 
+
+
   def show
   end
 
   private
+
+  def require_authorized_for_current_lesson
+    if current_user.enrolled_in?(current_lesson.section.course) != true
+      redirect_to course_path, alert: 'To view the Lessons in this course you need to first Enroll in the Course - Thank you'     
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
