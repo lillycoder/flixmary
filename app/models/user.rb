3fad8c3537f721566dc900c66c62e3873f703091
lillycoder <ltheinh@gmail.com>
2019-07-28 16:25:27 +0000

    finished with lesson 34

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :courses
   has_many :enrollments
-  has_many :enrolled_courses, through: :enrollment, source: :course
+  has_many :enrolled_courses, through: :enrollments, source: :course
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
