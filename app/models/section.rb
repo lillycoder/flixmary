de25ddfa23e77f05280901a5f4ff42e002fdbc19
lillycoder <ltheinh@gmail.com>
2019-08-13 22:48:51 +0000

    look to the finish line

--- app/models/section.rb
+++ app/models/section.rb
@@ -7,7 +7,7 @@ class Section < ApplicationRecord
 
   def next_section
     section = course.sections.where("row_order > ?", self.row_order).rank(:row_order).first
-    if lesson.blank? && section.next_section
+    if lessons.blank? && section.next_section
       return section.next_section.lessons.rank(:row_order).first
     end
     return section
