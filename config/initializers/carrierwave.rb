339ffc2c89cc3b2956fe975eee6da5a4a64a03bd
lillycoder <ltheinh@gmail.com>
2019-08-13 19:48:38 +0000

    trouble shooting invalid token error

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,12 +5,14 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
-    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],      # required
   }
-  config.fog_directory  = ENV["AWS_BUCKET"]              # required
+  config.fog_directory  = ENV["AWS_BUCKET"]  
+             # required
 
-  if Rails.env.development?
-    config.cache_dir = '/home/vagrant/uploads_tmp/tmp/uploads'
-    config.root = '/home/vagrant/uploads_tmp/tmp'
-  end
+  #if Rails.env.development?
+   # config.cache_dir = '/home/vagrant/uploads_tmp/tmp/uploads'
+   # config.root = '/home/vagrant/uploads_tmp/tmp'
+  #end
 end
+
