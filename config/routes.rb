cef5e25cac122e4be0cb999064c39e4739a9195f
lillycoder <ltheinh@gmail.com>
2019-08-13 00:13:12 +0000

    finished lesson 36

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,10 @@ Rails.application.routes.draw do
   end
   resources :lessons, only: [:show]
   namespace :instructor do
-    resources :sections, only: [:update]
     resources :lessons, only: [:update]
+    resources :sections, only: [:update] do
+      resources :lessons, only: [:create]
+    end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:create, :update] 
     end
